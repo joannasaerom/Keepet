3661a5469a4b2eebb63b02262fd6f3502e7bf942
Joanna Anderson <joanna.saerom.anderson@gmail.com>
2016-11-17 10:41:48 -0800

    Adds main page and page to display pets weight

---
+++ app/src/main/java/com/epicodus/pettracker/MainActivity.java
@@ -0,0 +1,43 @@
+package com.epicodus.pettracker;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.weightButton) Button mWeightButton;
+    @Bind(R.id.medicationButton) Button mMedicationButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+//        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+//        setSupportActionBar(myToolbar);
+
+        mWeightButton.setOnClickListener(this);
+        mMedicationButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v){
+        if(v == mWeightButton) {
+            Intent intent = new Intent(MainActivity.this, WeightActivity.class);
+            startActivity(intent);
+        } else if(v == mMedicationButton){
+            Intent intent = new Intent(MainActivity.this, )
+        }
+    }
+}

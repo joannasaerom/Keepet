a1f977adb676a3c0a4e6c3e20b1aafb86d244ae8
Joanna Anderson <joanna.saerom.anderson@gmail.com>
2016-11-30 11:48:57 -0800

    Creates Vet model

---
+++ app/src/main/java/com/epicodus/pettracker/Vet.java
@@ -0,0 +1,54 @@
+package com.epicodus.pettracker;
+
+import java.util.ArrayList;
+
+/**
+ * Created by joannaanderson on 11/30/16.
+ */
+
+public class Vet {
+    String mName;
+    String mPhone;
+    String mWebsite;
+    double mRating;
+    String mImageUrl;
+    ArrayList<String> mAddress = new ArrayList<>();
+    double mLatitude;
+    double mLongitude;
+
+    public Vet(String name, String phone, String website, double rating, String imageUrl, ArrayList<String> address, double latitude, double longitude){
+        this.mName = name;
+        this.mPhone = phone;
+        this.mWebsite = website;
+        this.mRating = rating;
+        this.mImageUrl = imageUrl;
+        this.mAddress = address;
+        this.mLatitude = latitude;
+        this.mLongitude = longitude;
+    }
+
+    public String getName(){
+        return mName;
+    }
+    public String getPhone(){
+        return mPhone;
+    }
+    public String getWebsite(){
+        return mWebsite;
+    }
+    public double getRating(){
+        return mRating;
+    }
+    public String getImageUrl(){
+        return mImageUrl;
+    }
+    public ArrayList<String> getAddress(){
+        return mAddress;
+    }
+    public double getLatitude(){
+        return mLatitude;
+    }
+    public double getmLongitude(){
+        return mLongitude;
+    }
+}
